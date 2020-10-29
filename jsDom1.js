f86677683136bf1764edd454a46439c89218137f
Julia Ballo <ballo.julia@gmail.com>
2020-10-28 17:42:24 -0700

    First Commit

---
+++ jsDom1.js
@@ -0,0 +1,11 @@
+//Exercise 53: Pickles Exercise
+document.getElementsByTagName("span")[0].innerHTML = "Disgusting";
+
+//Exercise 54: Manipulating Attributes Practice
+document.getElementById("egg").src = "https://www.flaticon.com/svg/static/icons/svg/3523/3523063.svg";
+document.getElementById("egg").alt = chicken;
+
+//Exercise 55: Magical Forest Circle Exercise
+document.getElementById("container").style.textAlign = 'center';
+document.getElementsByTagName("img")[0].style.width = "150px";
+document.getElementsByTagName("img")[0].style.borderRadius = "50%";
